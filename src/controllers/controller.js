f52adaa4bd910da9ce9a8cb5a3cbb1c556d71421
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-05 10:57:19 -0400

    feat(controller): add user model to controller

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,8 +1,10 @@
 const autoBind = require("auto-bind");
+const User = require("../models/userModel");
 
 module.exports = class {
   constructor() {
     autoBind(this);
+    this.User = User;
   }
 
   response({ res, message, code = 200, data = {} }) {
