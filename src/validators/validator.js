e0e78ecfefe3f214c08c9feb732b97b1e53e9cf4
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 09:17:11 -0400

    feat(validators): add validate middleware for error handling

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -1,7 +1,19 @@
 const autoBind = require("auto-bind");
+const { validationResult } = require("express-validator");
 
 module.exports = class {
   constructor() {
     autoBind(this);
+  }
+
+  validate(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Validation failed, entered data is incorrect",
+        data: [...errors.array().map((error) => error.msg)],
+      });
+    }
+    next();
   }
 };
