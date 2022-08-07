f3d3e9a7293e6af307aba3184b0367d63ea823cf
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:58:04 -0400

    feat(logging): add uncaught exception error handler

--- start/logging.js
+++ start/logging.js
@@ -1,5 +1,13 @@
+require("express-async-errors");
 const winston = require("winston");
+const debug = require("debug")("app:start:logging");
 
 module.exports = function () {
+  process.on("uncaughtException", (ex) => {
+    debug(ex);
+    winston.error(ex.message, ex);
+    process.exit(1);
+  });
+
   winston.add(new winston.transports.File({ filename: "logFile.log" }));
 };
