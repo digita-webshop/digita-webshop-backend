33864c4152543bd51649f0b9d63053f4db5ff21c
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:19:24 -0400

    feat(index route): add login middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const { isLoggedIn } = require("./../middlewares/auth");
 const authRouter = require("./auth");
 const adminRouter = require("./admin");
 const userRouter = require("./user");
 
 router.use("/auth", authRouter);
-router.use("/admin", adminRouter);
-router.use("/user", userRouter);
+router.use("/admin", isLoggedIn, adminRouter);
+router.use("/user", isLoggedIn, userRouter);
 
 module.exports = router;
