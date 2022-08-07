030a320e589175c7727cff9f0adda275c0774876
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:47:51 -0400

    feat(index route): add error middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { isLoggedIn, isAdmin } = require("./../middlewares/auth");
+const error = require("./../middlewares/error");
 const authRouter = require("./auth");
 const adminRouter = require("./admin");
 const userRouter = require("./user");
@@ -9,5 +10,7 @@ const userRouter = require("./user");
 router.use("/auth", authRouter);
 router.use("/admin", isLoggedIn, isAdmin, adminRouter);
 router.use("/user", isLoggedIn, userRouter);
+
+router.use(error);
 
 module.exports = router;
