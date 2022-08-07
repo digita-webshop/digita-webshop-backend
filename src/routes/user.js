66ed1954991dbf4795723f6317bbe90e06a2089a
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:03:52 -0400

    feat(user router): add dashboard route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("./controller");
 
-router.get("/", (req, res) => res.send("user router"));
+router.get("/", controller.dashboard);
 
 module.exports = router;
