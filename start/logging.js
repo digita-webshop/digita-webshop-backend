23c6f9d9951668ee5fe7a8f1ecdc24fb69db88ab
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:41:25 -0400

    feat(logging): create log file for error logging

--- start/logging.js
+++ start/logging.js
@@ -1,5 +1,4 @@
 const winston = require("winston");
-const debug = require("debug")("app:start:logging");
 
 module.exports = function () {
   winston.add(new winston.transports.File({ filename: "logFile.log" }));
