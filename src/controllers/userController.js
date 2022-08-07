de705b80431b96f58a5a9256a4f6d8680932760d
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:02:25 -0400

    feat(user controller): add dashboard controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,8 @@
+const controller = require("./controller");
+const _ = require("lodash");
+
+module.exports = new (class extends controller {
+  async dashboard(req, res) {
+    res.send("user dashboard");
+  }
+})();
