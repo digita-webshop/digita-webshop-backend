412d43d534bd5e5ae6b16aa992de0b5df8cf1d99
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:09:45 -0400

    fix(user router): edit controller route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./controller");
+const controller = require("./../controllers/userController");
 
 router.get("/", controller.dashboard);
 
