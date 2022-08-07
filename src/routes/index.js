ab0d2db6490654693f0f15a222b7f51a41020050
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-07 10:29:19 -0400

    feat(index route): add isAdmin to check the router admin access permission

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { isLoggedIn } = require("./../middlewares/auth");
+const { isLoggedIn, isAdmin } = require("./../middlewares/auth");
 const authRouter = require("./auth");
 const adminRouter = require("./admin");
 const userRouter = require("./user");
 
 router.use("/auth", authRouter);
-router.use("/admin", isLoggedIn, adminRouter);
+router.use("/admin", isLoggedIn, isAdmin, adminRouter);
 router.use("/user", isLoggedIn, userRouter);
 
 module.exports = router;
