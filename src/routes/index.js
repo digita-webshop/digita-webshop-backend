363fb124211e4e0c517a1149514ab144f856a537
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-10 00:06:10 -0400

    feat(index route): add isSuperAdmin to check the super admin router access permission

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { isLoggedIn, isAdmin } = require("./../middlewares/auth");
 const error = require("./../middlewares/error");
+const { isLoggedIn, isAdmin, isSuperAdmin } = require("./../middlewares/auth");
+
 const authRouter = require("./auth");
-const adminRouter = require("./admin");
 const userRouter = require("./user");
+const adminRouter = require("./admin");
+const superAdminRouter = require('./superAdmin')
 
 router.use("/auth", authRouter);
-router.use("/admin", isLoggedIn, isAdmin, adminRouter);
 router.use("/user", isLoggedIn, userRouter);
+router.use("/admin", isLoggedIn, isAdmin, adminRouter);
+router.use("/superAdmin", isLoggedIn, isAdmin, isSuperAdmin, superAdminRouter)
 
 router.use(error);
 
