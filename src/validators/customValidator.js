9cdd9f5ea3e39f21db6a7ee487d9096994323d69
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-15 18:26:27 -0400

    refactor(custom validation): rename & add validation checking the number of items

--- src/validators/customValidator.js
+++
@@ -1,7 +0,0 @@
-function emailValidator(e) {
-  const emailRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailRegex.test(e);
-}
-
-module.exports = { emailValidator };
