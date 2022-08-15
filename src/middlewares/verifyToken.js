993363b49bd2601b126ddb886317c5dc6d897cbb
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-14 22:02:58 -0400

    feat(verify): add verify user

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -14,3 +14,13 @@ export const verifyToken = (req, res, next) => {
     next();
   });
 };
+
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || req.user.role) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
