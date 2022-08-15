d9841a068b8a7f689909f75d0259a0b52c4951da
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-14 21:58:46 -0400

    feat(config): add cors, cookie parser package & add uploads middleware

--- start/config.js
+++ start/config.js
@@ -1,5 +1,11 @@
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
 module.exports = function (app, express) {
+  app.use(cors());
+  app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static("public"));
+  app.use("/uploads/images", express.static(path.join("uploads", "images")));
 };
