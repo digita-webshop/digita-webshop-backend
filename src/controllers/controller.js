ba5bbd47b66a76a298caad16bb02da5155407f5b
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-15 20:25:44 -0400

    feat(controller): add check params id method

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,10 +1,18 @@
 const autoBind = require("auto-bind");
-const User = require("../models/userModel");
+const User = require("../models/User");
+const mongoose = require("mongoose");
+const createError = require("./../utils/httpError");
 
 module.exports = class {
   constructor() {
     autoBind(this);
     this.User = User;
+  }
+
+  checkParamsId(id) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(createError(400, "Invalid id"));
+    }
   }
 
   response({ res, message, code = 200, data = {} }) {
