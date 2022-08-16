ae775b9ddd7bd938f74074c47927ade078e6ccea
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-15 20:21:59 -0400

    refactor(user model): edit the order schema

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { emailValidator } = require("../validators/custom");
+
+const addressSchema = new mongoose.Schema({
+  country: { type: String, trim: true },
+  state: { type: String, trim: true },
+  city: { type: String, trim: true },
+  street: { type: String, trim: true },
+  postalCode: { type: String, minlength: 10, unique: true },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
+    total: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: emailValidator,
+        message: "Invalid email format",
+      },
+    },
+    password: { type: String, required: true, minlength: 6 },
+    phone: { type: String, unique: true, minlength: 10 },
+    image: { type: String },
+    addresses: [addressSchema],
+    orders: [orderSchema],
+    wishlist: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
+    role: {
+      type: String,
+      enum: ["user", "admin", "superAdmin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
