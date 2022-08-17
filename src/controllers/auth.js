ca2016a1cb49b00da351942888b98a8e356ce83f
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-17 18:20:02 -0400

    feat(auth): add signup & login  for auth controller

---
+++ src/controllers/auth.js
@@ -0,0 +1,79 @@
+const controller = require("./controller");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+module.exports = new (class extends controller {
+  async signup(req, res) {
+    let newUser = await this.User.findOne({ email: req.body.email });
+    if (newUser) {
+      return this.response({
+        res,
+        code: 422,
+        message: "User exists already, please login instead.",
+      });
+    }
+
+    newUser = new this.User(
+      _.pick(req.body, ["userName", "email", "password"])
+    );
+
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
+
+    newUser.image = req.file.path
+    newUser.addresses = []
+    newUser.orders = []
+    newUser.wishlist = []
+
+    await newUser.save();
+
+    this.response({
+      res,
+      message: "User created successfully",
+      data: _.pick(user, ["_id", "userName", "email"]),
+    });
+  }
+
+  async login(req, res) {
+    const user = await this.User.findOne({ email: req.body.email });
+    if (!user) {
+      return this.response({
+        res,
+        code: 404,
+        message: "User not found!",
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isValidPassword) {
+      return this.response({
+        res,
+        code: 400,
+        message: "Wrong password or username!",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      config.get("jwt_key"),
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    const { password, role, ...otherDetails } = user._doc;
+
+    this.response({
+      res: res.cookie("access_token", token, {
+        httpOnly: true,
+      }),
+      message: "Login successful",
+      data: { details: { ...otherDetails }, role },
+    });
+  }
+})();
