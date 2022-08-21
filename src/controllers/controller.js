ca0332bfd6f93c939ea4b4dae60e606de6cef3df
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-20 23:00:38 -0400

    feat(controller): add Product, Article & Order Model

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,12 +1,18 @@
 const autoBind = require("auto-bind");
-const User = require("../models/User");
 const mongoose = require("mongoose");
 const createError = require("./../utils/httpError");
+const User = require("./../models/User");
+const Product = require("./../models/Product");
+const Article = require("./../models/Article");
+const Order = require("./../models/Order");
 
 module.exports = class {
   constructor() {
     autoBind(this);
     this.User = User;
+    this.Product = Product;
+    this.Article = Article;
+    this.Order = Order;
   }
 
   checkParamsId(id) {
