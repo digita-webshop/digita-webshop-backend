0dd630b81b7878eebc86dfc42f49f96869afd4a8
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-25 18:20:21 -0400

    refactor(utils): change of createError from OOP to Functional

--- src/utils/httpError.js
+++ src/utils/httpError.js
@@ -1,6 +1,13 @@
-module.exports = new (class extends Error {
-  constructor(message, errorCode) {
-    super(message);
-    this.code = errorCode;
-  }
-})();
+// module.exports = new (class extends Error {
+//   constructor(message, errorCode) {
+//     super(message);
+//     this.code = errorCode;
+//   }
+// })();
+const createError = (status, message) => {
+  const err = new Error();
+  err.status = status;
+  err.message = message;
+  return err;
+};
+module.exports = createError;
