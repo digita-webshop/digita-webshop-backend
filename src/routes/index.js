d48f786e9ece3c8136168228574b8586c6c1fabb
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-26 13:20:46 -0400

    refactor(index routes)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const { error } = require("./../middlewares/error");
-const {
-  verifyUser,
-  verifyAdmin,
-  verifySuperAdmin,
-} = require("./../middlewares/verifyToken");
-
 const authRoutes = require("./auth");
 const cartRoutes = require("./cart");
 const userRoutes = require("./users");
@@ -16,20 +9,36 @@ const articleRoutes = require("./articles");
 const productRoutes = require("./products");
 const superAdminRoutes = require("./superAdmin");
 
-router.use("/auth", authRoutes);
-router.use("/cart", verifyUser, cartRoutes);
-router.use("/articles", articleRoutes);
-router.use("/products", productRoutes);
-router.use("/user", verifyUser, userRoutes);
-router.use("/panel/admin", verifyUser, verifyAdmin, adminRoutes);
-router.use(
-  "/panel/superAdmin",
+const {
   verifyUser,
   verifyAdmin,
   verifySuperAdmin,
-  superAdminRoutes
-);
+} = require("./../middlewares/verifyToken");
 
+const error = require("./../middlewares/error");
+
+// Auth routes
+router.use("/auth", authRoutes);
+
+// Cart routes
+router.use("/cart", verifyUser, cartRoutes);
+
+// ARTICLES routes
+router.use("/articles", articleRoutes);
+
+// PRODUCTS routes
+router.use("/products", productRoutes);
+
+// USER routes
+router.use("/users", verifyUser, userRoutes);
+
+// ADMIN routes
+router.use("/admins", verifyAdmin, adminRoutes);
+
+// SUPER ADMIN routes
+router.use("/superAdmin", verifySuperAdmin, superAdminRoutes);
+
+// Error middleware
 router.use(error);
 
 module.exports = router;
