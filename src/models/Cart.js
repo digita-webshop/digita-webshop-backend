cc32c4c725bcc9c9c206285e6ab4284fab46a595
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-26 13:08:14 -0400

    feat(models): add cart model

---
+++ src/models/Cart.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    products: [
+      {
+        productId: { type: mongoose.Types.ObjectId, ref: "Product" },
+        quantity: Number,
+        name: String,
+        price: Number,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
