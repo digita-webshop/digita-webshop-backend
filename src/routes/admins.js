3f68edc230717a9b46b2ecdb50f8b8a672382d7f
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-26 13:01:58 -0400

    refactor(orders): add order routers to admin, super admin & delete an orders file

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -12,6 +12,12 @@ const {
 const fileUpload = require("../middlewares/fileUpload");
 const { verifySuperAdmin } = require("./../middlewares/verifyToken");
 
+const {
+  addOrder,
+  deleteOrder,
+  getOrdersByUserId,
+} = require("./../controllers/order");
+
 // CREATE ADMIN
 router.post("/", verifySuperAdmin, fileUpload.single("image"), createAdmin);
 
@@ -26,5 +32,17 @@ router.get("/:id", getAdmin);
 
 //GET ALL ADMINS
 router.get("/", verifySuperAdmin, getAdmins);
+
+// GET ORDERS BY USER ID
+router.get("/my-orders/:uid", getOrdersByUserId);
+
+// VALIDATOR FOR ADD ORDER
+const validator = require("./../validators/order");
+
+// ADD ORDER
+router.post("/order", validator.orderValidator(), validator.validate, addOrder);
+
+// DELETE ORDER
+router.delete("/orders/:oid", deleteOrder);
 
 module.exports = router;
--- src/routes/superAdmin.js
+++ src/routes/superAdmin.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./../controllers/superAdminController");
 
-router.get("/", controller.dashboard);
+const {
+  addOrder,
+  deleteOrder,
+  getOrdersByUserId,
+} = require("./../controllers/order");
+
+// GET ORDERS BY USER ID
+router.get("/my-orders/:uid", getOrdersByUserId);
+
+// VALIDATOR FOR ADD ORDER
+const validator = require("./../validators/order");
+
+// ADD ORDER
+router.post("/order", validator.orderValidator(), validator.validate, addOrder);
+
+// DELETE ORDER
+router.delete("/order/:oid", deleteOrder);
 
 module.exports = router;
