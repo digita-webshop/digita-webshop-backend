adec59f6fc53f642155871bfe0f3a71cc8fa33af
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-08-26 11:46:25 -0400

    fix(verify token): remove next arg when calling the verifyToken func

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,21 +2,21 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const createError = require("../utils/httpError");
 
-export const verifyToken = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
   }
 
-  jwt.verify(token, config.get("jwt_key"), (err, user) => {
-    if (err) next(createError(403, "Token is not valid!"));
+  jwt.verify(token, config.get("jwt_key"), async (err, user) => {
+    if (err) return next(createError(401, "Token is not valid!"));
     req.user = user;
     next();
   });
 };
 
-export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.role) {
       next();
     } else {
@@ -25,8 +25,8 @@ export const verifyUser = (req, res, next) => {
   });
 };
 
-export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
     if (req.user.role !== "user") {
       next();
     } else {
@@ -35,12 +35,18 @@ export const verifyAdmin = (req, res, next) => {
   });
 };
 
-export const verifySuperAdmin = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+const verifySuperAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
     if (req.user.role === "superAdmin") {
       next();
     } else {
       return next(createError(403, "You are not authorized!"));
     }
   });
+};
+
+module.exports = {
+  verifyUser,
+  verifyAdmin,
+  verifySuperAdmin,
 };
