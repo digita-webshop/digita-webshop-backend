697388a6c5035878e7e28e9bb1f1dfc9635262ad
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-09-20 10:08:52 -0400

    fix(order): clear check status, userId

--- src/validators/order.js
+++ src/validators/order.js
@@ -5,8 +5,6 @@ module.exports = new (class extends validator {
   orderValidator() {
     return [
       check("productId").not().isEmpty().withMessage("Product Id is required"),
-      check("status").not().isEmpty().withMessage("Status is required"),
-      check("userId").not().isEmpty().withMessage("User Id is required"),
     ];
   }
 })();
