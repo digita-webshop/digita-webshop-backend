5d3f8d6292714c98d310c693efd6f9089acc784b
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-09-21 11:18:43 -0400

    feat(controller): add Review model to constructor & pass "total" field in response

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,22 +4,25 @@ const Product = require("./../models/Product");
 const Article = require("./../models/Article");
 const Order = require("./../models/Order");
 const Cart = require("./../models/Cart");
+const Review = require("./../models/Review");
 
 module.exports = class {
   constructor() {
     autoBind(this);
-    this.User = User;
-    this.Product = Product;
-    this.Article = Article;
-    this.Order = Order;
     this.Cart = Cart;
+    this.User = User;
+    this.Order = Order;
+    this.Review = Review;
+    this.Article = Article;
+    this.Product = Product;
   }
 
-  response({ res, message, code = 200, data = {} }) {
+  response({ res, message, code = 200, data = {}, total }) {
     res.status(code).json({
       code,
       message,
       data,
+      total,
     });
   }
 };
