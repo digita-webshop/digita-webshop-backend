ea047493c582daf62d53f0eee30b16627cd93333
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-09-24 17:43:27 -0400

    feat: add a few lines of comments

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+/* Importing the express module. */
 const express = require("express");
 
-// express app
+/* Creating an express app. */
 const app = express();
+/* Importing the routes file. */
 const router = require("./src/routes");
 
+/* Importing the config, db and logging files. */
 require("./start/config")(app, express);
 require("./start/db")(app);
 require("./start/logging")();
 
 // routes
+/* Telling the app to use the router file for all requests that start with /api. */
 app.use("/api", router);
--- start/config.js
+++ start/config.js
@@ -1,11 +1,18 @@
+/* Importing the cors, cookie-parser, and path modules. */
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+/* This is a function that is exporting the middleware that is being used in the app. */
 module.exports = function (app, express) {
+  /* Allowing the server to accept requests from other domains. */
   app.use(cors());
+  /* Parsing the cookies that are being sent to the server. */
   app.use(cookieParser());
+  /* Parsing the JSON that is being sent to the server. */
   app.use(express.json());
+  /* This is parsing the URL encoded data that is being sent to the server. */
   app.use(express.urlencoded({ extended: true }));
+  /* This is allowing the server to access the images that are being uploaded to the server. */
   app.use("/uploads/images", express.static(path.join("uploads", "images")));
 };
