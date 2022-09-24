f99b33e9b91d9b4fd0a0c8d4e23710f33c51a83e
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-09-24 17:33:50 -0400

    fix: add password hashing for userUpdate & adminUpdate and giving access to admin, superAdmin for update them

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,3 +1,4 @@
+/* Importing the required modules. */
 const mongoose = require("mongoose");
 const controller = require("./controller");
 const _ = require("lodash");
@@ -5,7 +6,16 @@ const bcrypt = require("bcrypt");
 const createError = require("../utils/httpError");
 
 module.exports = new (class extends controller {
+  /**
+   * It creates a new admin user
+   * @param req - The request object.
+   * @param res - The response object
+   * @param next - This is a function that you call when you want to pass control to the next middleware
+   * function in the stack.
+   * @returns The response is being returned.
+   */
   async createAdmin(req, res, next) {
+    /* Checking if the admin exists already. */
     let newAdmin;
     try {
       newAdmin = await this.User.findOne({ email: req.body.email });
@@ -23,11 +33,13 @@ module.exports = new (class extends controller {
       });
     }
 
+    /* Creating a new user object. */
     newAdmin = await this.User(
       _.pick(req.body, ["userName", "email", "password"])
     );
 
     newAdmin.role = "admin";
+    /* Hashing the password. */
     try {
       const salt = await bcrypt.genSalt(10);
       newAdmin.password = await bcrypt.hash(newAdmin.password, salt);
@@ -45,6 +57,7 @@ module.exports = new (class extends controller {
       );
     }
 
+    /* Returning the response to the client. */
     return this.response({
       res,
       message: "Admin created",
@@ -52,10 +65,23 @@ module.exports = new (class extends controller {
     });
   }
 
+  /* It updates an admin */
   async updateAdmin(req, res, next) {
+    /* Checking if the id is valid or not. */
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return next(createError(400, "Invalid id"));
     }
+
+/* This is a middleware function that is checking if the user is a superAdmin or not. If the user is
+not a superAdmin, then it is checking if the user is trying to update his own profile or not. If the
+user is not trying to update his own profile, then it is returning an error. */
+    const userId = req.user.id;
+    if (req.user.role !== "superAdmin" && userId !== req.params.id) {
+      return next(
+        createError(401, "You are not authorized to update this user")
+      );
+    }
+    /* Updating the admin. */
     let updatedAdmin;
     try {
       updatedAdmin = await this.User.findByIdAndUpdate(
@@ -70,6 +96,8 @@ module.exports = new (class extends controller {
         createError(500, "Something went wrong, could not update admin.")
       );
     }
+
+    /* Checking if the admin exists or not. */
     if (!updatedAdmin) {
       return this.response({
         res,
@@ -77,17 +105,43 @@ module.exports = new (class extends controller {
         message: "Admin not found!",
       });
     }
+
+    /* Checking if the password is being updated or not. If it is being updated, then it is hashing the
+    password. */
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        updatedAdmin.password = await bcrypt.hash(updatedAdmin.password, salt);
+      } catch (err) {
+        return next(
+          createError(500, "Something went wrong, could not update a admin.")
+        );
+      }
+    }
+
+    /* Saving the updated admin to the database. */
+    try {
+      await updatedAdmin.save();
+    } catch (err) {
+      return next(
+        createError(500, "Something went wrong, could not update a admin.")
+      );
+    }
+
+    /* Returning the response to the client. */
     this.response({
       res,
       message: "Admin updated successfully",
-      data: _.pick(updatedAdmin, ["_id", "userName", "email"]),
+      data: _.pick(updatedAdmin, ["_id", "userName", "email", "phone"]),
     });
   }
 
+  /* It deletes an admin from the database */
   async deleteAdmin(req, res, next) {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return next(createError(400, "Invalid id"));
     }
+    /* Deleting an admin from the database. */
     let admin;
     try {
       admin = await this.User.findByIdAndDelete(req.params.id);
@@ -96,6 +150,8 @@ module.exports = new (class extends controller {
         createError(500, "Something went wrong, could not delete admin.")
       );
     }
+    /* Checking if the admin exists or not. If it does not exist, then it is returning a response to the
+client. */
     if (!admin) {
       return this.response({
         res,
@@ -110,10 +166,15 @@ module.exports = new (class extends controller {
     });
   }
 
+  /**
+   * It gets an admin by id
+   * @returns The admin object is being returned.
+   */
   async getAdmin(req, res, next) {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return next(createError(400, "Invalid id"));
     }
+    /* Getting an admin by id. */
     let admin;
     try {
       admin = await this.User.findById(req.params.id);
@@ -135,10 +196,16 @@ module.exports = new (class extends controller {
     });
   }
 
+  /**
+   * It gets all the admins from the database and returns them to the user
+   * @returns An array of admins and the total number of admins.
+   */
   async getAdmins(req, res, next) {
-    let admins;
+    /* Getting all the admins with their total number from the database. */
+    let admins, totalAdmins;
     try {
       admins = await this.User.find({ role: "admin" }, "-password");
+      totalAdmins = await this.User.countDocuments({ role: "admin" });
     } catch (err) {
       return next(
         createError(500, "Something went wrong, could not find admins.")
@@ -148,6 +215,7 @@ module.exports = new (class extends controller {
       res,
       message: "Admins found successfully",
       data: admins,
+      total: totalAdmins,
     });
   }
 })();
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const controller = require("./controller");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const createError = require("../utils/httpError");
 
 module.exports = new (class extends controller {
@@ -8,6 +9,15 @@ module.exports = new (class extends controller {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return next(createError(400, "Invalid id"));
     }
+
+    /* This is a middleware that checks if the user is authorized to update the user. */
+    const userId = req.user.id;
+    if (req.user.role === "user" && userId !== req.params.id) {
+      return next(
+        createError(401, "You are not authorized to update this user")
+      );
+    }
+
     let updatedUser;
     try {
       updatedUser = await this.User.findByIdAndUpdate(
@@ -28,10 +38,30 @@ module.exports = new (class extends controller {
         message: "User not found!",
       });
     }
+
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        updatedUser.password = await bcrypt.hash(updatedUser.password, salt);
+      } catch (err) {
+        return next(
+          createError(500, "Something went wrong, could not update a user.")
+        );
+      }
+    }
+
+    try {
+      await updatedUser.save();
+    } catch (err) {
+      return next(
+        createError(500, "Something went wrong, could not update a user.")
+      );
+    }
+
     this.response({
       res,
       message: "User updated successfully",
-      data: _.pick(updatedUser, ["_id", "userName", "email"]),
+      data: _.pick(updatedUser, ["_id", "userName", "email", "phone"]),
     });
   }
 
@@ -87,12 +117,13 @@ module.exports = new (class extends controller {
   async getUsers(req, res, next) {
     const pageNumber = parseInt(req.query.page) || 1;
     const nPerPage = parseInt(req.query.limit) || 20;
-    let users;
+    let users, totalUsers;
     try {
       users = await this.User.find({ role: "user" }, "-password")
         .sort({ _id: 1 })
         .skip((pageNumber - 1) * nPerPage)
         .limit(nPerPage);
+      totalUsers = await this.User.countDocuments({ role: "user" });
     } catch (err) {
       return next(
         createError(500, "Fetching users failed, please try again later.")
@@ -103,6 +134,7 @@ module.exports = new (class extends controller {
       res,
       message: "Users found successfully",
       data: users,
+      total: totalUsers,
     });
   }
 })();
